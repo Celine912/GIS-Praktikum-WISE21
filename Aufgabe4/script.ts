cee1dd493094d0d510e36830476155c42d704e7b
Celine912 <tuong.vy.celine.le@hs-furtwangen.de>
2021-12-10 16:14:38 +0100

    Drop

--- Aufgabe4/script.ts
+++ Aufgabe4/script.ts
@@ -1,107 +1,102 @@
 namespace Eventseite {
 
   class TodoElement {
-    interpret: string;
-    price: number;
-    date: Date;
-    
+      interpret: string;
+      price: number;
+      date: Date;
 
-    constructor(interpret: string, price: number, date: Date) {
-        this.interpret = interpret;
-        this.price = price;
-        this.date = date;
-        
-    }
-}
-
-
-  /*const inputFeld: HTMLInputElement = <HTMLInputElement>document.getElementById("input-Element");
-  const display: HTMLDivElement = <HTMLDivElement>document.getElementById("display");
-  */
+      constructor(interpret: string, price: number, date: Date) {
+          this.interpret = interpret;
+          this.price = price;
+          this.date = date;
+      }
+  }
 
   const interpretInput: HTMLInputElement = <HTMLInputElement>document.getElementById("interpret");
-  const priceInput: HTMLInputElement = <HTMLInputElement>document.getElementById("price");
   const datetimeInput: HTMLInputElement = <HTMLInputElement>document.getElementById("datetime");
+  const priceInput: HTMLInputElement = <HTMLInputElement>document.getElementById("price");
   const entereventButton: HTMLElement = <HTMLButtonElement>document.getElementById("enterevent");
-  const eventliste: HTMLElement = <HTMLElement>document.getElementById("eventliste");
+  const eventtabelle: HTMLElement = <HTMLElement>document.getElementById("eventtabelle");
 
   let elementID: number = 0;
 
-  
   entereventButton.addEventListener("click", enterEvent);
 
   if (localStorage.length > 0) {
-    load();
+      load();
   }
 
-  console.log("Test ausführen");
+  console.log("Test");
 
   function load(): void {
-    let index: number = 1;
-    for (let i: number = 1; i < localStorage.length; i++) {
+      let index: number = 0;
+      
+      for (let i: number = 0; i < localStorage.length; i++) {
 
-        while (localStorage.getItem(index.toString()) === null) {
-        index++;
-        }
+          while (localStorage.getItem(index.toString()) === null) {
+              index++;
+          }
 
-        let interpret: string = (JSON.parse(localStorage.getItem(index.toString()))).interpret;
-        let price: number = (JSON.parse(localStorage.getItem(index.toString()))).price;
-        let datetime: Date = (JSON.parse(localStorage.getItem(index.toString()))).datetime;
-        createEvent(interpret, price, datetime);
+          let interpret: string = interpretInput.value;
+          let price: number = parseInt(priceInput.value);
+          let datetime: Date = new Date(datetimeInput.value);
 
-        index++;
-    }
+          interpret = (JSON.parse(localStorage.getItem(index.toString()))).interpret;
+          price = (JSON.parse(localStorage.getItem(index.toString()))).price;
+          datetime = (JSON.parse(localStorage.getItem(index.toString()))).datetime;
+
+          index++;
+          createEvent(interpret, price, datetime);
+      }
   }
+  
+
   function enterEvent(): void {
-    let interpret: string = interpretInput.value;
-    let price: number = parseInt(priceInput.value);
-    let datetime: Date = new Date(datetimeInput.value);
-    console.log(interpret + "\n" + priceInput + "\n" + datetime);
+      let interpret: string = interpretInput.value;
+      let price: number = parseInt(priceInput.value);
+      let datetime: Date = new Date(datetimeInput.value);
+      console.log(interpret + "\n" + priceInput + "\n" + datetime);
 
+      createEvent(interpret, price, datetime);
 
-    let todoElement: TodoElement = new TodoElement(interpret, datetime, price);
-    let json: string = JSON.stringify(todoElement);
-    localStorage.setItem(elementID.toString(), json);
+      let todoElement: TodoElement = new TodoElement(interpret, price, datetime);
+      let json: string = JSON.stringify(todoElement);
+      localStorage.setItem(elementID.toString(), json);
 
-    createEvent(interpret, price, datetime);
-
-
-}
+  }
 
   function createEvent(interpret: string, price: number, datetime: Date): void {
       let tr: HTMLElement = document.createElement("tr");
       let interpretElement: HTMLElement = document.createElement("td");
       let priceElement: HTMLElement = document.createElement("td");
       let datetimeElement: HTMLElement = document.createElement("td");
-      let deleteElement: HTMLElement = document.createElement("td");
-      let deleteButton: HTMLElement = document.createElement("button");
+      let löschenElement: HTMLElement = document.createElement("td");
+      let löschenButton: HTMLElement = document.createElement("button");
 
-      tr.id = "delete" + elementID.toString();
-      deleteButton.id = elementID.toString();
+      tr.id = "löschen" + elementID.toString();
+      löschenButton.id = elementID.toString();
 
       interpretElement.innerText = interpret;
-      priceElement.innerText = price + "";
+      priceElement.innerText = price + "€";
       datetimeElement.innerText = datetime + "";
-      deleteButton.innerHTML = "delete";
+      löschenButton.innerHTML = "löschen";
 
-      deleteElement.append(deleteButton);
-      deleteElement.addEventListener("click", deleteevent);
+      löschenElement.append(löschenButton);
+      löschenElement.addEventListener("click", eventLöschen);
 
       tr.appendChild(interpretElement);
       tr.appendChild(priceElement);
       tr.appendChild(datetimeElement);
-      tr.appendChild(deleteElement);
+      tr.appendChild(löschenElement);
 
-      eventliste.appendChild(tr);
+      eventtabelle.appendChild(tr);
       elementID++;
   }
 
-  
+  function eventLöschen(eventLöschen: Event): void {
 
-  function deleteevent(deleteevent: Event): void {
-
-      let eventID: string = (<HTMLElement>deleteevent.target).id;
-      let tr: HTMLElement = document.getElementById("delete" + eventID);
+      let eventID: string = (<HTMLElement>eventLöschen.target).id;
+      let tr: HTMLElement = document.getElementById("löschen" + eventID);
       tr.remove();
       localStorage.removeItem(eventID.toString());
   }
